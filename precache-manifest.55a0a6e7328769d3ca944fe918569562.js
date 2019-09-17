4fdec321f94d9a27f8a5ad7b815ae1cd5cc4c697
jorge <jorgehasd@gmail.com>
2019-09-17 12:22:00 -0300

    Updates

---
+++ precache-manifest.55a0a6e7328769d3ca944fe918569562.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c7aa3fd75c766ad82becdd56b49ffe",
+    "url": "/assistencia/index.html"
+  },
+  {
+    "revision": "cbbdf0916cbfc0101cc8",
+    "url": "/assistencia/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "daa9bfdc24e48b92db46",
+    "url": "/assistencia/static/css/main.130ee1fb.chunk.css"
+  },
+  {
+    "revision": "cbbdf0916cbfc0101cc8",
+    "url": "/assistencia/static/js/2.ddea3462.chunk.js"
+  },
+  {
+    "revision": "daa9bfdc24e48b92db46",
+    "url": "/assistencia/static/js/main.e7bccf3f.chunk.js"
+  },
+  {
+    "revision": "e49b010ae87601731b33",
+    "url": "/assistencia/static/js/runtime~main.a53f648d.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/assistencia/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/assistencia/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/assistencia/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/assistencia/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/assistencia/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
